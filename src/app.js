18a9690ec7380bf30c66727501fc89159f10ce37
andresfacundo <andresfelipefacundo@gmail.com>
2025-01-15 09:35:14 -0500

    se habilitan cors
    se cambia la logica del FSP

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 
@@ -20,6 +23,7 @@ app.post('/calcular', (req, res) => {
   const cuatroSMLMV = salarioMinimo * 4;
   const diezSMLMV = salarioMinimo * 10;
   const auxilioDeTransporte = 200000;
+ 
 
   const totalRemuneracion = (tipoSalario === 'integral'
     ? otrosPagosSalariales + otrosPagosNoSalariales + auxilioAlimentacion + (salario * 0.7)
@@ -33,14 +37,20 @@ app.post('/calcular', (req, res) => {
   const ibc = tipoSalario === 'ordinario'
     ? otrosPagosSalariales + salario + excedente
     : tipoSalario === 'integral'
-      ? ((salario + otrosPagosSalariales) * 0.7) + excedente
-      : tipoSalario === 'medio tiempo' ? salarioMinimo : 0;
+    ? ((salario + otrosPagosSalariales) * 0.7) + excedente
+    : tipoSalario === 'medio tiempo' ? salarioMinimo : 0;
+
+  const ratio = ibc / salarioMinimo;
+  const porcentajeFSP = ((ratio >= 4 && ratio < 16) 
+  ? 1 : (ratio >= 16 && ratio <= 17) ? 1.2 : (ratio > 17 && ratio <= 18) 
+  ? 1.4 : (ratio > 18 && ratio <= 19) ? 1.6 : (ratio > 19 && ratio <= 20) 
+  ? 1.8 : (ratio > 20) ? 2 : 0) / 100;
 
   const saludEmpleador = salario + otrosPagosSalariales >= diezSMLMV ? 0.085 * ibc : 0;
   const saludTrabajador = ibc * 0.04;
   const pensionTrabajador = pensionado === 'No' ? ibc * 0.04 : 0;
   const pensionEmpleador = pensionado === 'No' ? ibc * 0.12 : 0;
-  const FSP = pensionado === 'No' ? ibc * 0.00 : 0;
+  const FSP = pensionado === 'No' ? ibc * porcentajeFSP : 0;
   const riesgosLaborales = Math.ceil(ibc * 0.00522);
   const sena = (salario + otrosPagosSalariales) >= diezSMLMV ? (salario + otrosPagosSalariales) * 0.02 : 0;
   const icbf = (salario + otrosPagosSalariales) >= diezSMLMV ? (salario + otrosPagosSalariales) * 0.03 : 0;
@@ -60,7 +70,7 @@ app.post('/calcular', (req, res) => {
   // Proyectado
 
   const provisionesPrestacionesSociales = Math.round(primaServicios + cesantias + interesesCesantias + vacaciones);
-  const aportesEmpleador = Math.round(saludEmpleador + pensionEmpleador + FSP + riesgosLaborales + sena + icbf + cajaCompensacion);
+  const aportesEmpleador = Math.round(saludEmpleador + pensionEmpleador + riesgosLaborales + sena + icbf + cajaCompensacion);
   const aportesTrabajador = saludTrabajador + pensionTrabajador + FSP;
   const retencionFuente = 2000000;
 
@@ -81,6 +91,7 @@ app.post('/calcular', (req, res) => {
       saludEmpleador,
       pensionTrabajador,
       pensionEmpleador,
+      FSP,
       riesgosLaborales,
       sena,
       icbf,
