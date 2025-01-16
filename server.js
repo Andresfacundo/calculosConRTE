8845286280f148c3de8816e571016f4d72778200
andresfacundo <andresfelipefacundo@gmail.com>
2025-01-16 15:58:53 -0500

    se agregan cabezeras

--- server.js
+++ server.js
@@ -6,14 +6,24 @@ const calculatorRoutes = require("./src/routes/calculatorRoutes");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.use(cors());
+
+app.use(cors({
+    origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.json({message: 'Server is running'})
+});
 
 app.use("/api", calculatorRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0',() => {
+    console.log(`Servidor corriendo en port: ${PORT}`);
     
 });
 
-module.exports = { app, PORT };
+module.exports =  app ;
