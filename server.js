90b7cfc659ad30b21c56f4e039f73995d225fa35
andresfacundo <andresfelipefacundo@gmail.com>
2025-04-04 11:48:46 -0500

    se cambio url de despliegue front

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-    origin: 'https://calculos-rte-frond-end.vercel.app',
+    origin: 'https://calculadora-gap-six.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
